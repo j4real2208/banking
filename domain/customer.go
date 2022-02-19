3d3d28ee2d832358dfdb9f2627b04a5dc2f8139d
j4real2208 <jojojohnsonibm@gmail.com>
2022-02-19 14:02:34 +0530

    Add: Dto feature impl.

--- domain/customer.go
+++ domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/j4real2208/banking/errs"
+import (
+	"github.com/j4real2208/banking/dto"
+	"github.com/j4real2208/banking/errs"
+)
 type Customer struct {
 	Id string			`db:"customer_id"`
 	Name string	
@@ -8,6 +11,25 @@ type Customer struct {
 	Zipcode string
 	DateofBirth string	`db:"date_of_birth"`
 	Status string
+}
+// Statu as Text method
+func ( c Customer ) statusAsText() string {
+	statusAsText := "active"
+	if c.Status == "0"{
+		statusAsText="inactive"
+	}
+	return statusAsText
+}
+// Dto conversion method 
+func (c Customer) ToDto() (dto.CustomerResponse)  {
+	return dto.CustomerResponse{
+		Id:				c.Id,
+		Name:			c.Name,
+		City:			c.City,
+		Zipcode:		c.Zipcode,
+		DateofBirth:	c.DateofBirth,
+		Status:			c.statusAsText(),
+	}	
 }
 
 type CustomerRepository interface {
--- service/customerService.go
+++ service/customerService.go
@@ -2,11 +2,12 @@ package service
 
 import (
 	"github.com/j4real2208/banking/domain"
+	"github.com/j4real2208/banking/dto"
 	"github.com/j4real2208/banking/errs"
 )
 type CustomerService interface {
 	GetAllCustomer(string) ([]domain.Customer , *errs.AppError)
-	GetCustomer(string) (*domain.Customer , *errs.AppError)
+	GetCustomer(string) (*dto.CustomerResponse , *errs.AppError)
 }
 
 type DefaultCustomerService struct {
@@ -25,8 +26,14 @@ func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer
 }
 
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer ,*errs.AppError) {
-	return s.repo.ByID(id)
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse,*errs.AppError) {
+	c, err :=  s.repo.ByID(id)
+	if err != nil {
+		return nil, err
+	}
+	response:= c.ToDto()
+	return &response, nil
+
 }
 
 func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService{
