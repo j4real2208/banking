9e0ce0b3766c92b049c823fbd852d949f0d925ce
j4real2208 <jojojohnsonibm@gmail.com>
2022-02-19 13:09:41 +0530

    Add: Introduce sqlx to code base.

--- domain/CustomerRepositoryDb.go
+++ domain/CustomerRepositoryDb.go
@@ -7,18 +7,20 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/j4real2208/banking/errs"
 	"github.com/j4real2208/banking/logger"
+	"github.com/jmoiron/sqlx"
 )
 
 
 type CustomerRepositoryDb struct {
-	client *sql.DB
+	client *sqlx.DB
 }
 
 func (d CustomerRepositoryDb ) ByID(id string) (*Customer, *errs.AppError) {
 	customerSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers where customer_id = ?"
-	row:= d.client.QueryRow(customerSql,id)
+	
 	var c Customer
-	err:= row.Scan(&c.Id,&c.Name,&c.City,&c.Zipcode,&c.DateofBirth,&c.Status)
+	err := d.client.Get(&c,customerSql,id)
+	
 	if err != nil {
 		if err==sql.ErrNoRows{
 			logger.Error("Error while scanning customer" + err.Error())
@@ -33,40 +35,29 @@ func (d CustomerRepositoryDb ) ByID(id string) (*Customer, *errs.AppError) {
 }
 
 func (d CustomerRepositoryDb) FindAll(status string) ([]Customer , *errs.AppError) {
-
-var rows *sql.Rows
-var err error
+	
+	
+	var err error
+	customers := make([]Customer , 0 )
 if status == ""{
 	findAll := "select * from customers"
-	rows,err = d.client.Query(findAll)
+	err = d.client.Select(&customers,findAll)
+	// rows,err = d.client.Query(findAll)
 }else{
 	findAll := "select * from customers where status = ?"
-	rows,err = d.client.Query(findAll,status)
+	err = d.client.Select(&customers,findAll,status)
 }
 // Query from Sql 
 if err != nil {
-	logger.Error("Hit an error in querying the rows map in sql possible db outage"+err.Error())
+	logger.Error("Hit an error in querying the rows of customer table in sql possible db outage"+err.Error())
 	return nil,errs.NewUnexpectedError("Unexpected error from db side")
-}
-
-// Iterating through the table 
-customers := make([]Customer , 0 )
-
-for rows.Next() {
-	var c Customer
-	err := rows.Scan(&c.Id,&c.Name,&c.City,&c.Zipcode,&c.DateofBirth,&c.Status)
-	if err != nil {
-		logger.Error("Hit an error in querying a record of the customers "+err.Error())
-		return nil,errs.NewUnexpectedError("Unexpected error from db side")
-	}
-	customers = append(customers, c)
 }
 
 return customers,nil
 }
 
 func NewCustomerRepositoryDb() CustomerRepositoryDb{
-	client, err := sql.Open("mysql", "root:codecamp@tcp(localhost:3306)/banking")
+	client, err := sqlx.Open("mysql", "root:codecamp@tcp(localhost:3306)/banking")
 	if err != nil {
 		panic(err)
 	}
--- domain/customer.go
+++ domain/customer.go
@@ -2,11 +2,11 @@ package domain
 
 import "github.com/j4real2208/banking/errs"
 type Customer struct {
-	Id string
-	Name string
+	Id string			`db:"customer_id"`
+	Name string	
 	City string
 	Zipcode string
-	DateofBirth string
+	DateofBirth string	`db:"date_of_birth"`
 	Status string
 }
 
